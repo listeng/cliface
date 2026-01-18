cb912dae48e68fb8f4e6b33f6e849522f11785fb
tls <aosp@163.com>
2026-01-18 22:57:49 +0800

    Update build configuration for macOS arm64 and remove Windows GUI flag

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 CGO_ENABLED=1 GOOS=darwin GOARCH=amd64 go build -o cliface-darwin-amd64 .
-CGO_ENABLED=1 GOOS=windows GOARCH=amd64 CC=x86_64-w64-mingw32-gcc go build -ldflags="-H windowsgui=0" -o cliface-windows-amd64.exe .
+CGO_ENABLED=1 GOOS=windows GOARCH=amd64 CC=x86_64-w64-mingw32-gcc go build -o cliface-windows-amd64.exe .
